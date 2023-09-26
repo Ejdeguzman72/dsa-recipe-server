f90f4efe2e1ccb5696ac03c7e30dddd24ad79a84
Ejdeguzman72 <ejdeguzman72@gmail.com>
2023-09-26 11:25:46 -0400

    adding sql file

---
+++ src/main/resources/sql-scripts/recipe-type-sql-scripts.sql
@@ -0,0 +1,2 @@
+insert into recipe_type (descr)
+values ('American'),('Breakfeast'),('Brazillian'),('Comfort'),('Deserts'),('Filipino'),('HIspanic'),('Italian'),('Seafood'),('Slow Cooker'),('Asian');
